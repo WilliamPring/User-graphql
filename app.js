3600d1a18c6c8d3aba53235643d6e05cab0a69da
WilliamPring <mrwilliampring@gmail.com>
2020-04-02 20:18:11 -0400

    Adding schema and realtionship

--- app.js
+++ app.js
@@ -7,21 +7,43 @@ export default function() {
 
     const typeDefs = `
 
+    type User {
+        userName: String
+        born: Date
+        name: String
+        restaurants: [Restaurant] @relation(name: "ATE_AT", direction: "OUT")
+    }
     type Cuisine{
       type: String
     }
+    type Restaurant {
+        name: String
+        priceRange: String
+        info: String
+        cuisines: [Cuisine] @relation(name: "TYPE_OF", direction: "OUT")
+        location: Municipality @relation(name: "IS_IN", direction: "OUT")
+    }
 
+    type Municipality {
+        city: String
+        provienceState: String
+        country: Country @relation(name: "EXIST", direction: "OUT")
+    }
+
+    type Country {
+        country: String
+        municipalities: [Municipality] @relation(name: "EXIST", direction: "IN")
+    }
 
     type Query {
         "A simple type for getting started!"
         hello: String
       }
 
-
     `;
     const resolvers = {
         Query: {
-          hello: () => 'world',
+          hello: () => 'world'
         },
       };
 
