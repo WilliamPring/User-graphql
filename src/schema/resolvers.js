9b319c5a85789958d936d5a56115b0a5583f8fa8
WilliamPring <mrwilliampring@gmail.com>
2020-04-04 12:03:15 -0400

    Updated my schema for better relationships and naming

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -2,4 +2,7 @@ export default ({
     Query: {
       hello: () => 'world'
     },
+    Mutation: {
+
+    }
 })
--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -8,7 +8,7 @@ const typeDefs = `
         reviews: [Review] @relation(name: "HAS_POST", direction: "OUT")
     }
     type Review {
-        foods: [String],
+        foods: [String]
         starRating: Float
         reviewSummary: String
         images: [Image] @relation(name: "HAS_IMAGE", direction: "OUT")
@@ -21,30 +21,47 @@ const typeDefs = `
     type Cuisine{
       type: String
     }
+    type City {
+        name: String
+        municipality: Municipality @relation(name: "IS_IN", direction: "OUT")
+    }
     type Restaurant {
         name: String
         priceRange: String
         info: String
         cuisines: [Cuisine] @relation(name: "TYPE_OF", direction: "OUT")
-        location: Municipality @relation(name: "IS_IN", direction: "OUT")
+        location: City @relation(name: "LOCATED_AT", direction: "OUT")
         reviews: [Review] @relation(name: "FOOD_REVIEW", direction: "IN")
     }
 
     type Municipality {
-        city: String
         provienceState: String
         country: Country @relation(name: "EXIST", direction: "OUT")
+        cities: [City] @relation(name: "IS_IN", direction: "IN")
     }
 
     type Country {
-        country: String
+        name: String
         municipalities: [Municipality] @relation(name: "EXIST", direction: "IN")
     }
 
     type Query {
         "A simple type for getting started!"
         hello: String
-      }
+    }
 
+    input CountryInput {
+        countryName: String!
+    }
+
+    input MunicipalityInput {
+        cityName: String
+        provienceState: String
+        countryName: String
+    }
+
+    type Mutation {
+        createCountry(input: CountryInput!) : Country
+    }
     `;
 export default typeDefs;
